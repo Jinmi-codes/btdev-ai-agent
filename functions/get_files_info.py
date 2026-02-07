c7536cb7eae06429b823e9401235f6a2b3dcba96
Jinmi-codes <jinmiolaosoko@gmail.com>
2026-02-07 23:58:55 +0100

    add functions

---
+++ functions/get_files_info.py
@@ -0,0 +1,29 @@
+import os.path
+import os
+def get_files_info(working_directory, directory='.'):
+	try:
+		abs_path = os.path.abspath(working_directory)
+		target_dir = os.path.normpath(os.path.join(abs_path, directory))
+		allowed_target = os.path.commonpath([abs_path, target_dir]) == abs_path 
+		if not allowed_target:
+			return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+		if not os.path.isdir(target_dir):
+			return f'Error: "{directory}" is not a directory'
+
+		dir_list = os.listdir(target_dir)
+		full_data = []
+		for item in dir_list:
+			name = item
+			file_size = os.path.getsize(os.path.join(target_dir, item))
+			is_dir = True
+			if not os.path.isdir(os.path.join(target_dir, item)):
+				is_dir = False
+			data = f"- {name}: file_size={file_size} bytes, is_dir={"True" if is_dir else "False"}"
+			full_data.append(data)
+		info = "\n".join(full_data)
+		return info
+	except Exception as e:
+		return f"Error: {e}"
+
+
+print(get_files_info("../../", "ai-agent"))
---
+++ functions/write_file.py
@@ -0,0 +1,22 @@
+import os
+import os.path
+
+def write_file(working_directory, file_path, content):
+    try:
+        abs_path = os.path.abspath(working_directory)
+        target_file = os.path.normpath(os.path.join(abs_path, file_path))
+        allowed = os.path.commonpath([abs_path, target_file]) == abs_path
+
+        if not allowed:
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+        if os.path.isdir(target_file):
+            return f'Error: Cannot write to "{file_path}" as it is a directory'
+        if not os.path.isdir(os.path.dirname(target_file)):
+            os.makedirs(os.path.dirname(target_file), exist_ok=True)
+
+        with open(target_file, 'w') as file:
+            file.write(content)
+        
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f"Error: {e}"
